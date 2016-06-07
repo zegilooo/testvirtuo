2eec3e1b75ec6aab00126830e22d87da186416ba
Gilles Eschenbrenner <zegilooo@gmail.com>
2016-06-07 21:55:40 +0000

    add mongoose vehicle model

---
+++ models/vehicle.js
@@ -0,0 +1,19 @@
+"use strict";
+const mongoose  = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/testvirtuo', function(err) {
+    if (err) throw err;
+});
+
+const vehicleSchema = new mongoose.Schema ({
+  "plate": { type: String, required: true, trim: true },
+  "mileage": Number,
+  "fuel": Number,
+  "gps": {
+    "latitude": Number,
+    "longitude": Number
+  },
+  "capture_at":  { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('vehicle',vehicleSchema,'vehicle');
