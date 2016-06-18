d950e88e6f4154dc33b2666e47220555d37fc335
Gilles Eschenbrenner <zegilooo@gmail.com>
2016-06-18 13:57:34 +0200

    -low-- linter corrections

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const vehicleSchema = new mongoose.Schema ({
-  "plate": { type: String, required: true, trim: true },
-  "mileage": Number,
-  "fuel": Number,
-  "gps": {
-    "latitude": Number,
-    "longitude": Number
+  'plate': { type: String, required: true, trim: true },
+  'mileage': Number,
+  'fuel': Number,
+  'gps': {
+    'latitude': Number,
+    'longitude': Number
   },
-  "capture_at":  { type: Date, default: Date.now }
+  'capture_at':  { type: Date, default: Date.now }
 })
 
 const vehicleModule = mongoose.model('vehicle',vehicleSchema,'vehicle')
--- testvirtuo.js
+++ testvirtuo.js
@@ -1,53 +1,53 @@
-import express from "express"
-import bodyParser  from "body-parser"
-import vehicle from "./models/vehicle"
+import express from 'express'
+import bodyParser  from 'body-parser'
+import vehicle from './models/vehicle'
 
 const app         =   express()
 const router      =   express.Router()
 
-const infotypes   =   ["fuel","mileage","coordinates"]
+const infotypes   =   ['fuel','mileage','coordinates']
 
 app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({"extended" : false}))
+app.use(bodyParser.urlencoded({'extended' : false}))
 
-router.route("/vehicles")
+router.route('/vehicles')
     .get((req,res) => {
         let response = {}
         vehicle.find({},(err,data) => {
             if(err) {
-                response = {"error" : true,"message" : "Error fetching data : " + err}
+                response = {'error' : true,'message' : 'Error fetching data : ' + err}
             } else {
                 response =  data
             }
             res.json(response)
         })
     })
-router.route("/vehicle")
+router.route('/vehicle')
     .post((req,res) => {
         let db = new vehicle(req.body)
         let response = {}
         db.save((err) => {
             if(err) {
-                response = {"error" : true,"message" : "Error adding data : " + err}
+                response = {'error' : true,'message' : 'Error adding data : ' + err}
             } else {
-                response = {"error" : false,"message" : "Data added"}
+                response = {'error' : false,'message' : 'Data added'}
             }
             res.json(response)
         })
     })
-router.route("/vehicle/:plate/:infotype")
+router.route('/vehicle/:plate/:infotype')
     .get((req,res) => {
         let infotype = req.params.infotype
         if(infotypes.indexOf(infotype)===-1){
-            res.json({"error" : true,"message" : "Error infotype : " + infotype})
+            res.json({'error' : true,'message' : 'Error infotype : ' + infotype})
         }else{
             let response = {}
-            if (infotype==="coordinates") infotype = "gps" 
-            vehicle.find({"plate" : req.params.plate}, infotype + " capture_at" ,(err,data) => {
+            if (infotype==='coordinates') { infotype = 'gps' }
+            vehicle.find({'plate' : req.params.plate}, infotype + ' capture_at' ,(err,data) => {
                 if(err) {
-                    response = {"error" : true,"message" : "Error fetching data : " + err}
+                    response = {'error' : true,'message' : 'Error fetching data : ' + err}
                 } else {
                     response =  data
                 }
